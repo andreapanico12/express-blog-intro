e9957c36dcd259a2cfa34ff8b4713a5cd1ef60dc
andreapanico12 <andreapanico12@gmail.com>
2024-12-17 14:08:20 +0100

    feat: add public folder and import images, create posts array with 5 objects

--- server.js
+++ server.js
@@ -2,6 +2,39 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+const posts = [
+  {
+    titolo: "Ciambellone",
+    contenuto: "Questo è il contenuto di Ciambellone",
+    immagine:"/images/ciambellone.jpeg",
+    tags:["ciambellone","dolci","lievitati"]
+  },
+  {
+    titolo: "Cracker alla barbabietola",
+    contenuto: "Questo è il contenuto di Cracker alla barbabietola",
+    immagine:"/images/cracker_barbabietola.jpeg",
+    tags:["cracker","aperitivo","barbabietola","merenda"]
+  },
+  {
+    titolo: "Pane fritto dolce",
+    contenuto: "Questo è il contenuto di Pane fritto dolce",
+    immagine:"/images/pane_fritto_dolce.jpeg",
+    tags:["pane", "fritto","dolci","lievitati","cinese"]
+  },
+  {
+    titolo: "Pasta in crema di barbabietola",
+    contenuto: "Questo è il contenuto di Pasta in crema di barbabietola",
+    immagine:"/images/pasta_barbabietola.jpeg",
+    tags:["pasta","primi","primi piatti", "barbabietola", "vegano"]
+  },
+  {
+    titolo: "Torta Paesana",
+    contenuto: "Questo è il contenuto di Torta Paesana",
+    immagine:"/images/torta_paesana.jpeg",
+    tags:["canditi","dolci","cacao","frutta secca"]
+  }
+]
+
 
 
 // http://localhost:3001/
